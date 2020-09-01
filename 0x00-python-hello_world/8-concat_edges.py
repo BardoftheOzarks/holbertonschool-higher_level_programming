dc7e191c230a7c053fe86349e507a08c8f9b3c44
BardoftheOzarks <1886@holbertonschool.com>
2020-09-01 03:12:28 +0000

    added task 9

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = '{0:39.26} {0:110.4} {0:.5}'.format(str)
 print(str)
