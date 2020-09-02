aed2f82478b0425b541fafb804e53cc92b12a992
BardoftheOzarks <1886@holbertonschool.com>
2020-09-02 00:42:38 +0000

    added task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newstring =''
+    for char in (str):
+        c = ord(char)
+        if c > 96 and c < 123:
+            newstring+= chr(c - 32)
+        else:
+            newstring+= chr(c)
+    print(newstring)
