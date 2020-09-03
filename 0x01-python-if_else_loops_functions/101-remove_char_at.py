6a638bd1a1cd20a5f2c3ddc7e868f6a1c7c08417
BardoftheOzarks <1886@holbertonschool.com>
2020-09-03 00:27:33 +0000

    retested

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,8 +3,8 @@ def remove_char_at(str, n):
     i = 0
     while i < len(str):
         if i == n:
-            i++
+            i = i + 1
         else:
             new_str += str[i]
-            i++
+            i = i + 1
     return new_str
