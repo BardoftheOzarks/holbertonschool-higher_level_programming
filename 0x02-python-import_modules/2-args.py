6d61a078329ee35ef2d63cdbc5cd2c29b6f7d6c6
BardoftheOzarks <1886@holbertonschool.com>
2020-09-03 21:31:35 +0000

    added 0 argument factor

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     ac = len(argv) - 1
-    if ac == 1:
+    if ac == 0:
+        print('0 arguments.')
+    elif ac == 1:
         print('1 argument:')
     else:
         print('{:d} arguments:'.format(ac))
