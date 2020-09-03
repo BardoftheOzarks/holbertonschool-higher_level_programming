dd011196e91085fb1dc4049ddc54dbbcc1768e39
BardoftheOzarks <1886@holbertonschool.com>
2020-09-03 21:25:38 +0000

    import guarded

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-ac = len(argv) - 1
-if ac == 1:
-    print('1 argument:')
-else:
-    print('{:d} arguments:'.format(ac))
-for i in range(1, ac + 1):
-    print('{:d}: {}'.format(i, argv[i]))
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    ac = len(argv) - 1
+    if ac == 1:
+        print('1 argument:')
+    else:
+        print('{:d} arguments:'.format(ac))
+        for i in range(1, ac + 1):
+            print('{:d}: {}'.format(i, argv[i]))
