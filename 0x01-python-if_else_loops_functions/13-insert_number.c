e032d20595d20c7cd27e8011af658f12df8156b7
BardoftheOzarks <1886@holbertonschool.com>
2020-09-03 11:10:09 +0000

    cleanup

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * insert_node - inserts a node in a list
+ * @head: start of list
+ * @number: number to be filed
+ * Return: address of new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	while
+}
