df155a5ce063f778d2e3bcf353f1abd74e568c06
BardoftheOzarks <1886@holbertonschool.com>
2020-09-04 04:48:50 +0000

    added ByteCode task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1.py import add, sub, mul, div
+    from sys import argv
+    argc = len(argv)
+    if agrc != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    
