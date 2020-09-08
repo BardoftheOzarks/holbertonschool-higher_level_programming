fd52b075537dbc34cf2d459dafa4740d8f46eca6
BardoftheOzarks <1886@holbertonschool.com>
2020-09-08 21:11:35 +0000

    first push

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for char in my_string:
+        if char is not 'c' and char is not 'C':
+            new_string += char
+    return new_string
