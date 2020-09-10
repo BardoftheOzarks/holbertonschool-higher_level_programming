dbf4f661df1722bd2f6a995af6444ad51bdb8ba5
BardoftheOzarks <1886@holbertonschool.com>
2020-09-10 23:58:18 +0000

    added through task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    best_score = 0
+    for name, score in a_dictionary.items():
+        if score > best_score:
+            best = name
+            best_score = score
+    return best
