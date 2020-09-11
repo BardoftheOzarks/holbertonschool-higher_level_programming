e5648adf3728b1ac363c7c8a974579481cb2e652
BardoftheOzarks <1886@holbertonschool.com>
2020-09-11 01:43:05 +0000

    adjusted inaugural if statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    best = ''
-    best_score = 0
-    for name, score in a_dictionary.items():
-        if score > best_score:
-            best = name
-            best_score = score
-    return best
+    if a_dictionary:
+        best = ''
+        best_score = 0
+        for name, score in a_dictionary.items():
+            if score > best_score:
+                best = name
+                best_score = score
+        return best
