d8dc108f71d8db925eb9848dce42199b97151e64
BardoftheOzarks <1886@holbertonschool.com>
2020-09-21 20:46:45 +0000

    added task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for ele in range(list_length):
+        try:
+            result.append(my_list_1[ele] / my_list_2[ele])
+        except ZeroDivisionError:
+            print('division by 0')
+            result.append(0)
+        except TypeError:
+            print('wrong type')
+            result.append(0)
+        except IndexError:
+            print('out of range')
+            result.append(0)
+    return result
