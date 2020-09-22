8ef084ae29cb32e8d15c16ee667287d299175296
BardoftheOzarks <1886@holbertonschool.com>
2020-09-22 23:18:11 +0000

    added task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Creates a class with a single attribute that returns square'''
+
+
+class Square:
+    '''A class with a single private instance attribute "size"'''
+    def __init__(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
