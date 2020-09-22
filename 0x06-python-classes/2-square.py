c282b2b74b76daaeac0dc2bb1e9ccfee24d44386
BardoftheOzarks <1886@holbertonschool.com>
2020-09-22 22:26:57 +0000

    added task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+'''Creates a class with a single attribute'''
+
+
 class Square:
+    '''A class with a single private instance attribute "size"'''
     def __init__(self, size=0):
-        try:
-            self._Square__size = size
-        except TypeError
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self._Square__size = size
