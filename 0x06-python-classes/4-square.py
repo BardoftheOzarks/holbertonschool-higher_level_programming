03c154992f2427cdf38980ebee2bad123adbbc42
BardoftheOzarks <1886@holbertonschool.com>
2020-09-22 23:25:02 +0000

    moved area to properties section

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     '''A class with a single private instance attribute "size"'''
-    def __init__(self, size):
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
@@ -15,6 +15,9 @@ class Square:
     def size(self):
         return self.__size
 
+    def area(self):
+        return self.__size**2
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -22,6 +25,3 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
-
-    def area(self):
-        return self.__size**2
