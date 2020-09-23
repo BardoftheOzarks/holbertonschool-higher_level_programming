6aaff21cb53977c0c0bd7d78c904ff788047e297
BardoftheOzarks <1886@holbertonschool.com>
2020-09-23 03:01:43 +0000

    added task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+'''Creates a class with two attributes'''
+
+
+class Square:
+    '''A class with two private instance attributes'''
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if type(position) is not tuple or len(position) is not 2\
+           or type(position[0]) is not int or type(position[1]) is not int\
+           or position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__size = size
+        self.__position = position
+
+    @property
+    def position(self):
+        return self.__position
+
+    @property
+    def size(self):
+        return self.__size
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size is 0:
+            print()
+        else:
+            for o in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for spaces in range(self.__position[0]):
+                    print(' ', end='')
+                for hashes in range(self.__size):
+                    print('#', end='')
+                print()
+
+    def __repr__(self):
+        strang = ''
+        if self.__size > 0:
+            for o in range(self.__position[1]):
+                strang += '\n'
+            for i in range(self.__size):
+                for spaces in range(self.__position[0]):
+                    strang += ' '
+                for hashes in range(self.__size):
+                    strang += '#'
+                if (i + 1) < self.__size:
+                    strang += '\n'
+        return strang
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) is not 2\
+           or type(value[0]) is not int or type(value[1]) is not int\
+           or value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
