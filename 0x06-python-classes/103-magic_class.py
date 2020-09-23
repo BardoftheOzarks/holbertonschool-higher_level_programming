4b8f9aa448b3d1d091dd849dd33a1bfa5d4499ec
BardoftheOzarks <1886@holbertonschool.com>
2020-09-23 04:54:44 +0000

    added return to init

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,11 +6,12 @@ class MagicClass:
     '''Stores a radius and computes either area
     or circumference of a circle'''
 
-    def __init__(self, radius=0):
+    def __init__(self, radius):
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self._MagicClass__radius = radius
+        return None
 
     def area(self):
         return self._MagicClass__radius**2 * math.pi
