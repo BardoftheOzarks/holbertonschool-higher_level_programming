06277168a3dfab97231a8da89b37ef7664a37c5e
BardoftheOzarks <1886@holbertonschool.com>
2020-09-28 21:43:51 +0000

    added first half of project

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Creates an empty class'''
+
+
+class Rectangle:
+    '''An empty class'''
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''Creates a Rectangle class'''
+
+
+class Rectangle:
+    '''A class with height and width'''
+    def __init__(self, width=0, height=0):
+        self._Rectangle__width = width
+        self._Rectangle__height = height
+
+    @property
+    def width(self):
+        return self._Rectangle__width
+
+    @property
+    def height(self):
+        return self._Rectangle__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self._Rectangle__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self._Rectangle__height = value
+
+    def area(self):
+        return self._Rectangle__height * self._Rectangle__width
+
+    def perimeter(self):
+        return ((self._Rectangle__height + self._Rectangle__width) * 2)
