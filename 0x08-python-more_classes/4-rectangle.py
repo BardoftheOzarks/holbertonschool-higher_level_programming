8d5ad0b6a70d92a9e6b494860f6b781d12ba5ff5
BardoftheOzarks <1886@holbertonschool.com>
2020-09-28 23:44:26 +0000

    added conditionals for init

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -5,14 +5,6 @@
 class Rectangle:
     '''A class with height and width'''
     def __init__(self, width=0, height=0):
-        if type(width) is not int:
-            raise TypeError('width must be an integer')
-        if width < 0:
-            raise ValueError('width must be >= 0')
-        if type(height) is not int:
-            raise TypeError('height must be an integer')
-        if height < 0:
-            raise ValueError('height must be >= 0')
         self._Rectangle__width = width
         self._Rectangle__height = height
 
@@ -39,3 +31,20 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self._Rectangle__height = value
+
+    def area(self):
+        return self._Rectangle__height * self._Rectangle__width
+
+    def perimeter(self):
+        return ((self._Rectangle__height + self._Rectangle__width) * 2)
+
+    def __str__(self):
+        sting = ''
+        for h in range(self._Rectangle__height):
+            for w in range(self._Rectangle__width):
+                sting += '#'
+            if (h + 1) < self._Rectangle__height:
+                sting += '\n'
+        return sting
+
+        
