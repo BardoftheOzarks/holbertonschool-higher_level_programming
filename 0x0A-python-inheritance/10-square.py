474612db55e94fdc7fa46848e6df3048092f1d21
BardoftheOzarks <1886@holbertonschool.com>
2020-10-07 01:05:03 +0000

    adjusted for super()

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,17 +7,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator('size', size)
-        '''self._Rectangle__width = size
-        self._Rectangle__height = size'''
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
-
-    @property
-    def _Rectangle__width(self):
-        return self.__size
-
-    @property
-    def _Rectangle__height(self):
-        return self.__size
