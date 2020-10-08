6a0a5a9292999fa46867f592274d019841a47e3e
BardoftheOzarks <1886@holbertonschool.com>
2020-10-08 14:36:43 +0000

    removed extra line if whole file read

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,6 +6,6 @@ def read_lines(filename="", nb_lines=0):
     '''How many lines do you want'''
     with open(filename) as f:
         if nb_lines <= 0:
-            print(f.read())
+            print(f.read(), end='')
         for i in range(nb_lines):
             print(f.readline(), end='')
