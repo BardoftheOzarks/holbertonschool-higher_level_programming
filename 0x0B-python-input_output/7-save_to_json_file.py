233c712b2d553eabe9a406720d3eb87016b30e67
BardoftheOzarks <1886@holbertonschool.com>
2020-10-08 04:14:37 +0000

    added file open line

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,4 +5,5 @@
 def save_to_json_file(my_obj, filename):
     '''dumps to a file'''
     import json
-    json.dump(my_obj, filename)
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
