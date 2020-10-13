6624b7a422670aa906f846273f5a34979a9fef38
BardoftheOzarks <1886@holbertonschool.com>
2020-10-13 23:33:19 +0000

    added from_json_string, create, and load_from_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,12 +29,33 @@ class Base:
         dictionaries = []
         for obj in list_objs:
             dictionaries.append(obj.to_dictionary())
-        with open(cls.__name__ + '.json', 'w') as f:
+        with open('{}.json'.format(cls.__name__), 'w') as f:
             f.write(cls.to_json_string(dictionaries))
 
-'''    @staticmethod
+    @staticmethod
     def from_json_string(json_string):
+        '''translates json object to a string'''
         import json
+        string_list = json.loads(json_string)
+        if string_list is None or len(string_list) == 0:
+            return '[]'
+        return string_list
 
-        if json.dumps(json_string) is None or json.dumps(json)
-'''
+    @classmethod
+    def create(cls, **dictionary):
+        '''creates an instance from a dictionary'''
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        '''Creates a list of instances from a file'''
+        insta_list = []
+        try:
+            with open('{}.json'.format(cls.__name__)) as f:
+                for obj in cls.from_json_string(f.read()):
+                    insta_list.append(cls.create(**obj))
+        except Exception:
+            pass
+        return insta_list
