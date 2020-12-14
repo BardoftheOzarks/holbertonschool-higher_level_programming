8847b046647df9c5d1e81f3cfa3e81a7d95ea3ac
BardoftheOzarks <1886@holbertonschool.com>
2020-12-14 17:15:55 +0000

    task 3 worked without change

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Lists matching states from argument in database hbtn_0e_0_usa'''
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    my_host = 'localhost'
+    db = MySQLdb.connect(host=my_host, port=3306,
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3])
+    if len(argv) == 6:
+        state = '{} {}'.format(argv[4], argv[5])
+    else:
+        state = argv[4]
+    c = db.cursor()
+    c.execute('SELECT * FROM states ORDER BY id ASC')
+    rows = c.fetchall()
+    for row in rows:
+        if row[1] == state:
+            print(row)
+    c.close()
+    db.close()
