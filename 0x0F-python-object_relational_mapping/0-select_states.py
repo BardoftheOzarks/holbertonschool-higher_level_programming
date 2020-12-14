e89647313950dcad3c45a22bddab83c14a233212
BardoftheOzarks <1886@holbertonschool.com>
2020-12-14 00:25:42 +0000

    added Tim's suggestions

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port=3306,
+    my_host = 'localhost'
+    db = MySQLdb.connect(host=my_host, port=3306,
                          user=argv[1], passwd=argv[2],
                          db=argv[3])
     c = db.cursor()
