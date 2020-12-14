c334590daadc7c416edc3792bab769cb5e7c702a
BardoftheOzarks <1886@holbertonschool.com>
2020-12-14 00:23:08 +0000

    still no password

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,10 @@
 '''Lists all states from database hbtn_0e_0_usa'''
 
 
+import MySQLdb
+from sys import argv
+
 if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2],
                          db=argv[3])
