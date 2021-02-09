e8b82220639bb3df3ea8b4c077e1647b4058ee7d
BardoftheOzarks <1886@holbertonschool.com>
2021-02-09 05:58:40 +0000

    shortened

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 const newlist = list.map((i, x) => i * x);
-
-//for (let count = 0; list[count]; count++) {
-//  newlist[count] = list[count] * count;
-//}
 console.log(list);
 console.log(newlist);
