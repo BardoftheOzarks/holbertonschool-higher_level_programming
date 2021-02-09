65fbd3b11ac2497dceed1ad9271d1259dbc9b38c
BardoftheOzarks <1886@holbertonschool.com>
2021-02-09 05:55:41 +0000

    reformated for map method

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newlist = [];
+const newlist = list.map((i, x) => i * x);
 
-for (let count = 0; list[count]; count++) {
-  newlist[count] = list[count] * count;
-}
+//for (let count = 0; list[count]; count++) {
+//  newlist[count] = list[count] * count;
+//}
 console.log(list);
 console.log(newlist);
