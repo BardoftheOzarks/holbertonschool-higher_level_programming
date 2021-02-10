da2872e8dd53aac898ea38318b6a6fddbc4d9606
BardoftheOzarks <1886@holbertonschool.com>
2021-02-10 05:18:20 +0000

    added all mandatory

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const movielist = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < movielist.length; i++) {
+      for (let j = 0; j < movielist[i].characters.length; j++) {
+	if (movielist[i].characters[j].includes('18')) {
+	  count++;
+	}
+      }
+    }
+    console.log(count);
+  }
+});
---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'UTF-8', function (err) {
+      if (err) {
+	console.log(err);
+      }
+    });
+  }
+});
