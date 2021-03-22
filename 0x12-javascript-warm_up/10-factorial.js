0cf71c5a0041fb16921720dd1bd0ff7b841ed160
BardoftheOzarks <1886@holbertonschool.com>
2021-03-22 18:36:09 +0000

    added through task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-const num = process.argv[2]
+const integer = process.argv[2];
 
-function factorialize(num) {
-  if (isNaN(num)) {
-    
+function factorialize (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  } else if (num < 0) {
+    return -1;
+  } else {
+    return (num * factorialize(num - 1));
+  }
+}
+console.log(factorialize(integer));
