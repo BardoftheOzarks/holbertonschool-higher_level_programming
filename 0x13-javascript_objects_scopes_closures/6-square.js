db6a18a40580406ad7e3b541a7f8c4c0d321ce86
BardoftheOzarks <1886@holbertonschool.com>
2021-03-23 19:22:06 +0000

    semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,14 +2,14 @@
 const oldSquare = require('./5-square');
 
 class Square extends oldSquare {
-    charPrint(c) {
-	if (c === undefined) {
-	    c = 'X';
-	}
-	for (let i = 0; i < this.width; i++) {
-	    console.log(c.repeat(this.width));
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
