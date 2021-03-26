8fbba37a3985388ac6cb56c36181e3d41124e20c
BardoftheOzarks <1886@holbertonschool.com>
2021-03-26 15:45:17 +0000

    added task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists all cities contained in database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ORDER BY cities.id ASC
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states on states.id=cities.state_id ORDER BY cities.id ASC
