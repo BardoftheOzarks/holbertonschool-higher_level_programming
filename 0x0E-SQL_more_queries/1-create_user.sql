0e28a9d4c601091ec3a09a258666d2be849eeb71
BardoftheOzarks <1886@holbertonschool.com>
2021-03-26 14:37:12 +0000

    moved comment section

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creates server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+-- grants all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
