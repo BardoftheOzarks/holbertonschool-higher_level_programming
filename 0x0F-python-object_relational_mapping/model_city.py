0226df52d3e4d2d0cf4a388bf4707fbbf0eaf5a7
BardoftheOzarks <1886@holbertonschool.com>
2021-03-29 17:03:27 +0000

    pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ Class definition of City
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 
+
 class City(Base):
     '''class to link to table "cities"'''
     __tablename__ = 'cities'
