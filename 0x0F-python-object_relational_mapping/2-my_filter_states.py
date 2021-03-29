efe9becc465a3c6d33457f0613038aae481929c1
BardoftheOzarks <1886@holbertonschool.com>
2021-03-29 00:14:45 +0000

    moved port assignment to end of statement

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    db = MySQLdb.connect('localhost', port=3306, argv[1], argv[2], argv[3])
+    db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3],
+                         port=3306)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name='{}'\
