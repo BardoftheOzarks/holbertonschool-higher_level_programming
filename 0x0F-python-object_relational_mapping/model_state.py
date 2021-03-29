ad81e88543ef4763b9bded8bef38bb0517000f6c
BardoftheOzarks <1886@holbertonschool.com>
2021-03-29 05:00:18 +0000

    added task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,4 +11,4 @@ class State(Base):
     '''class to link to table "states"'''
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name =  Column(String[128], nullable=False)
+    name = Column(String(128), nullable=False)
