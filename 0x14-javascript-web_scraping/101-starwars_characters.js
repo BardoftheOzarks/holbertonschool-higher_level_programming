cfb0a7e9c076cf743f7215650756b4a586327856
BardoftheOzarks <1886@holbertonschool.com>
2021-05-05 05:23:16 +0000

    final test

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,13 +6,19 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
       console.log(err);
     } else {
       const characters = JSON.parse(body).characters;
+      let characters_sorted = {}
       for (const link in characters) {
         request(characters[link], (err, response, body) => {
           if (err) {
             console.log(err);
           } else {
-            const name = JSON.parse(body).name;
-            console.log(name);
+            let name = JSON.parse(body).name;
+            characters_sorted[link] = name;
+          }
+          if (Object.values(characters_sorted).length === characters.length) {
+	    for (let i = 0; i < characters.length; i++) {
+		console.log(characters_sorted[i]);
+            }
           }
         });
       }
